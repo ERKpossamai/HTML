d996ad05cdecf6d153653eb9f4079df976242e16
luquinhasprogamer08 <lucas.milites.camargo@escola.pr.gov.br>
2025-05-23 11:42:27 -0300

    alteração

---
+++ MeuTCC/galeria.js
@@ -0,0 +1,5 @@
+// Interação com botão "Saiba Mais"
+    document.getElementById("saibaMaisBtn").addEventListener("click", function() {
+    alert("Obrigado por visitar! Confira nossos produtos!!!. 🚀");
+window.location.href = 'produto.html';
+});
