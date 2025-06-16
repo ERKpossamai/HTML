39c4338f9362163a5c6467fca188ed4e1f0cef3b
Erick Possamai <erick.lemos@escola.pr.gov.br>
2025-06-16 10:48:28 -0300

    começando a loja

--- MeuTCC/js/script.js
+++ MeuTCC/js/script.js
@@ -136,3 +136,30 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+// Filtro por categoria
+document.querySelectorAll('.menu button').forEach(button => {
+  button.addEventListener('click', () => {
+    const categoria = button.getAttribute('data-categoria');
+    document.querySelectorAll('.produto').forEach(produto => {
+      if (categoria === 'todos' || produto.classList.contains(categoria)) {
+        produto.style.display = 'flex';
+      } else {
+        produto.style.display = 'none';
+      }
+    });
+  });
+});
+
+// Filtro por pesquisa
+document.getElementById('campo-pesquisa').addEventListener('input', () => {
+  const termo = document.getElementById('campo-pesquisa').value.toLowerCase();
+  document.querySelectorAll('.produto').forEach(produto => {
+    const marca = produto.getAttribute('data-marca').toLowerCase();
+    if (marca.includes(termo)) {
+      produto.style.display = 'flex';
+    } else {
+      produto.style.display = 'none';
+    }
+  });
+});
